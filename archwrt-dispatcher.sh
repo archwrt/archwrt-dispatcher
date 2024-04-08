18d8cad61f8ff1a28b1c71bff0677bfda6b38fee
edward-p <edward@edward-p.xyz>
2024-04-08 21:46:49 +0800

    format code

--- archwrt-dispatcher.sh
+++ archwrt-dispatcher.sh
@@ -2,9 +2,9 @@
 dispacher_dir=/etc/archwrt/dispatcher
 . "$dispacher_dir/dispatcher.conf"
 
-if ! modinfo xt_FULLCONENAT &> /dev/null; then
-  # fallback to MASQUERADE
-  use_fullconenat = "false"
+if ! modinfo xt_FULLCONENAT &>/dev/null; then
+	# fallback to MASQUERADE
+	use_fullconenat = "false"
 fi
 
 start_services() {
@@ -16,7 +16,7 @@ start_services() {
 
 stop_services() {
 	for s in "${services[@]}"; do
-        systemctl stop "$s"
+		systemctl stop "$s"
 	done
 }
 
@@ -33,66 +33,81 @@ cf_redir() {
 		ipset -! add cloudflare "${net}" nomatch
 	done
 
-    iptables -t nat -N CLOUDFLARE
-    iptables -t nat -I PREROUTING -j CLOUDFLARE
-    iptables -t nat -I OUTPUT -j CLOUDFLARE
+	iptables -t nat -N CLOUDFLARE
+	iptables -t nat -I PREROUTING -j CLOUDFLARE
+	iptables -t nat -I OUTPUT -j CLOUDFLARE
 
-    iptables -t nat -A CLOUDFLARE -p tcp -m set --match-set cloudflare dst -m comment --comment "Cloudflare IP" -j DNAT --to-destination "${cf_dest}"
+	iptables -t nat -A CLOUDFLARE -p tcp -m set --match-set cloudflare dst -m comment --comment "Cloudflare IP" -j DNAT --to-destination "${cf_dest}"
 }
 
 down() {
-  stop_services
-  cat /usr/share/iptables/empty-{filter,nat,mangle}.rules | iptables-restore -w
+	stop_services
+	cat /usr/share/iptables/empty-{filter,nat,mangle}.rules | iptables-restore -w
 }
 
 up() {
 	case $1 in
-		default)
-			wan=$(ip route | \
-				grep default | \
-				grep -o 'dev.*' | \
-				awk '{print $2}')
-			[ -z "$wan" ] && { echo 'Read default interface from route failed!' 1>&2;  exit 1; }
-			;;
-		*)
-			wan=$1
-			;;
+	default)
+		wan=$(ip route |
+			grep default |
+			grep -o 'dev.*' |
+			awk '{print $2}')
+		[ -z "$wan" ] && {
+			echo 'Read default interface from route failed!' 1>&2
+			exit 1
+		}
+		;;
+	*)
+		wan=$1
+		;;
 	esac
 
-  #NAT
+	#NAT
 
-  if [[ "$wan" =~ "ppp" ]]; then
-    iptables -w -t mangle -A FORWARD -o $wan -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-  fi
+	if [[ "$wan" =~ "ppp" ]]; then
+		iptables -w -t mangle -A FORWARD -o $wan -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
+	fi
 
-  if [ -n "${lan}" ] && [ -n "${lan_net}" ] && [ "${iptables_no_masq}" != "true" ]; then
-    if [ "${use_fullconenat}" = "true"  ]; then
-	    iptables -w -t nat -A PREROUTING -i $wan -j FULLCONENAT
-      iptables -w -t nat -A POSTROUTING -d $lan_net -o $lan -j FULLCONENAT
-      iptables -w -t nat -A POSTROUTING -s $lan_net -o $wan -j FULLCONENAT
-    else
-      iptables -w -t nat -A POSTROUTING -d $lan_net -o $lan -j MASQUERADE
-  	  iptables -w -t nat -A POSTROUTING -s $lan_net -o $wan -j MASQUERADE
-    fi
-  fi
+	if [ -n "${lan}" ] && [ -n "${lan_net}" ] && [ "${iptables_no_masq}" != "true" ]; then
+		if [ "${use_fullconenat}" = "true" ]; then
+			iptables -w -t nat -A PREROUTING -i $wan -j FULLCONENAT
+			iptables -w -t nat -A POSTROUTING -d $lan_net -o $lan -j FULLCONENAT
+			iptables -w -t nat -A POSTROUTING -s $lan_net -o $wan -j FULLCONENAT
+		else
+			iptables -w -t nat -A POSTROUTING -d $lan_net -o $lan -j MASQUERADE
+			iptables -w -t nat -A POSTROUTING -s $lan_net -o $wan -j MASQUERADE
+		fi
+	fi
 
 	# Filter Rules
 	# iptables
-	sed "s/\$wan/$wan/g;s/\$lan/$lan/g;s/\$allowed_ports/$allowed_ports/g" "$dispacher_dir/filter.rules" \
-		| iptables-restore -w || { echo Error: Loading filter.rules failed; down; exit 1; }
+	sed "s/\$wan/$wan/g;s/\$lan/$lan/g;s/\$allowed_ports/$allowed_ports/g" "$dispacher_dir/filter.rules" |
+		iptables-restore -w || {
+		echo Error: Loading filter.rules failed
+		down
+		exit 1
+	}
 	#ip6tables
-	sed "s/\$wan/$wan/g;s/\$lan/$lan/g;s/\$allowed_ports/$allowed_ports/g" "$dispacher_dir/filter6.rules" \
-		| ip6tables-restore -w || { echo Error: Loading filter.rules failed; down; exit 1; }
+	sed "s/\$wan/$wan/g;s/\$lan/$lan/g;s/\$allowed_ports/$allowed_ports/g" "$dispacher_dir/filter6.rules" |
+		ip6tables-restore -w || {
+		echo Error: Loading filter.rules failed
+		down
+		exit 1
+	}
 
 	if [ -n "$cf_dest" ]; then
-    cf_redir || { echo "Error: Cloudflare redirect failed" 1>&2; down; exit 1; }
+		cf_redir || {
+			echo "Error: Cloudflare redirect failed" 1>&2
+			down
+			exit 1
+		}
 	fi
 
 	start_services
 
-  if [ -n "$dns_hijack" ]; then
-    iptables -w -t nat -I PREROUTING -s $lan_net -p udp -m udp --dport 53 -m comment --comment dns_redir -j DNAT --to-destination $dns_hijack
-  fi
+	if [ -n "$dns_hijack" ]; then
+		iptables -w -t nat -I PREROUTING -s $lan_net -p udp -m udp --dport 53 -m comment --comment dns_redir -j DNAT --to-destination $dns_hijack
+	fi
 }
 
 while [ $# -gt 0 ]; do
@@ -122,7 +137,7 @@ while [ $# -gt 0 ]; do
 				foo - use foo as wan interface
 				bar - use bar as wan interface
 		EOF
-        exit 0
+		exit 0
 		;;
 	esac
 done
